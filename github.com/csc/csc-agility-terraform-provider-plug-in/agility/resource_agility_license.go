44622eed38cd6122d59e5eb52d9f7e8e95f02704
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-10-09 13:30:50 +0530

    Update resource_agility_license.go

--- github.com/csc/csc-agility-terraform-provider-plug-in/agility/resource_agility_license.go
+++ github.com/csc/csc-agility-terraform-provider-plug-in/agility/resource_agility_license.go
@@ -9,43 +9,46 @@ import (
 	"github.com/csc/csc-agility-terraform-provider-plug-in/agility/api"
 	"github.com/hashicorp/terraform/helper/schema"
 
+	"encoding/json"
+
+
 )
 
 type SMLicense struct{
-	XMLName struct{}    "SMLicense"
-	XMLNS 		string "xmlns,attr,omitempty"
-	LicenseVersion 	string	"LicenseVersion"
-	Licensee		string	"Licensee"
-	NumberOfClients	string	"NumberOfClients"
-	MaxPermittedInstances	string"MaxPermittedInstances"
-	DBQueryType		string	"DBQueryType"
-	Signature		string 	"Signature"
+	XMLName struct{}    `"SMLicense"`
+	XMLNS 		string `"xmlns,attr,omitempty"`
+	LicenseVersion 	string	`"LicenseVersion"`
+	Licensee		string	`"Licensee"`
+	NumberOfClients	string	`"NumberOfClients"`
+	MaxPermittedInstances	string `"MaxPermittedInstances"`
+	DBQueryType		string	`"DBQueryType"`
+	Signature		string 	`"Signature"`
 }
 type Adapters struct{
-	XMLName struct{}    "Adapters"
-	Adapter string		"Adapter"
+	XMLName struct{}    `"Adapters"`
+	Adapter string		`"Adapter"`
 
 }
 type Modules struct{
-	XMLName struct{}    "Modules"
-	Module string		"Module"
+	XMLName struct{}    `"Modules"`
+	Module string		`"Module"`
 }
 type IssueDate struct{
-	XMLName struct{}	"IssueDate"
-	IssueDay	string	"IssueDay"
-	IssueMonth	string	"IssueMonth"
-	IssueYear	string	"IssueYear"
+	XMLName struct{}	`"IssueDate"`
+	IssueDay	string	`"IssueDay"`
+	IssueMonth	string	`"IssueMonth"`
+	IssueYear	string	`"IssueYear"`
 }
 type ExpiryDate struct{
-	XMLName struct{}	"ExpiryDate"
-	ExpiryDay	string	"ExpiryDay"
-	ExpiryMonth	string	"ExpiryMonth"
-	ExpiryYear	string	"ExpiryYear"
+	XMLName struct{}	`"ExpiryDate"`
+	ExpiryDay	string	`"ExpiryDay"`
+	ExpiryMonth	string	`"ExpiryMonth"`
+	ExpiryYear	string	`"ExpiryYear"`
 }
 type ServerNodeLock struct{
-	XMLName struct{}	"ServerNodeLock"
-	Mask	string	"Mask"
-	IPAddr	string	"IPAddr"
+	XMLName struct{}	`"ServerNodeLock"`
+	Mask	string	`"Mask"`
+	IPAddr	string	`"IPAddr"`
 }
 
 func resourceLicenseUpload() *schema.Resource {
@@ -60,112 +63,125 @@ func resourceLicenseUpload() *schema.Resource {
 			"licenseversion": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"licensee": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
 			},
-			"adapters": &schema.Schema{
+			"adapter": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: 	true,
-				Computed: 	false,
+				Required: true,
 				ForceNew:	true,
 			},
-			"modules": &schema.Schema{
+			"module": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"issueday": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"issuemonth": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"issueyear": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"expiryday": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"expirymonth": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"expiryyear": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"numberofclients": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"maxpermittedinstances": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"mask": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"ipaddr": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"dbquerytype": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 			"signature": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: 	true,
-				Computed: 	false,
 				ForceNew:	true,
+
 			},
 		},
 	}
 }
 
+
+func init() {
+	file, err1 := os.Open("./agility/api/conf.json")
+	if err1 != nil {
+		log.Println("error:", err1)
+	}
+	decoder := json.NewDecoder(file)
+	configuration = Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		log.Println("error:", err)
+	}
+}
+
 func LicenseUpload(ResourceData *schema.ResourceData, meta interface{}) error {
+	credentials = meta.(ProvCredentials)
 	f, errf := os.OpenFile("./agility/api/agility.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 	if errf != nil {
 		log.Println("error opening file: ", errf)
 	}
 	defer f.Close()
-
 	log.SetOutput(f)
-	licenseresponse, _ := api.LicenseUpload(credentials.UserName, credentials.Password)
+
+	licenseresponse := api.LicenseUpload(credentials.UserName, credentials.Password)
 	r := strings.NewReader(string(licenseresponse))
 	log.Println("Deploy response is : ", r)
 
