c9371aed07ae3dab93ba9e4a9387c0a3860a0bd3
asingh1007 <32540749+asingh1007@users.noreply.github.com>
2017-10-10 12:15:47 +0530

    Add files via upload

---
+++ github.com/csc/csc-agility-terraform-provider-plug-in/Variables.tf
@@ -0,0 +1,108 @@
+variable "userid" {
+    description="The Username of the Agility Platform"
+    default = "pm"
+}
+variable "password" {
+    description="The Username of the Agility Platform"
+    default = "07June1990@"
+}
+variable "headcontainername" {
+    description="The name of the Parent Container",
+    default ="Group1A"
+}
+variable "parentcontainer1" {
+    default="HR"
+}
+variable "parentcontainer2" {
+    default="IT"
+}
+variable "parentcontainer3" {
+    default="Finance"
+}
+variable "subcontainer1" {
+    default="SubHR"
+}
+variable "subcontainer2" {
+    default="Admin"
+}
+variable "subcontainer3" {
+    default="Developer"
+}
+variable "project1" {
+    default="HR"
+}
+variable "project2" {
+    default = "Finance"
+}
+variable "project3" {
+    default = "Windows"
+}
+variable "project4" {
+    default = "Linux"
+}
+variable "project5" {
+    default = "DotNet"
+}
+variable "project6" {
+    default = "Java"
+}
+variable "project7" {
+    default = "Python"
+}
+variable "environment1" {
+    default = "Dev"
+}
+variable "environment2" {
+    default = "UAT"
+}
+variable "environment3" {
+    default = "Production"
+}
+variable "environment1_type" {
+    default = "DEV"
+}
+variable "environment2_type" {
+    default ="UAT"
+} 
+variable "environment3_type" {
+    default = "PROD"
+}
+variable "cloud_name" {
+    default = "AWS-EC2"
+}
+variable "cloud_type" {
+    default = "Amazon EC2 Cloud"
+}
+variable "hostname" {
+    default = "ec2.eu-west-1.amazonaws.com"
+}
+variable "aws_accesskey" {
+    default = "AKIAJI4INTOM6BXDWR3A"
+}
+variable "aws_secretkey" {
+    default = "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"
+}
+variable "aws_accountnumber" {
+    default = "531227887946"
+}
+variable "deployement_name" {
+    default = "depname"
+}
+variable "active" {
+    default = "true"
+}
+variable "version" {
+    default = "1"
+}
+variable "type" {
+    default = "XS"
+}
+variable "blueprint_name" {
+    default = "RHEL"
+}
+variable "environment_name" {
+    default = "test"
+}
+variable "project_name" {
+    default = "Testing"
+}
