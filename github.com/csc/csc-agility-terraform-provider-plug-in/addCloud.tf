dffb47c8ead21287349167cba845197dc7f3562f
asingh1007 <32540749+asingh1007@users.noreply.github.com>
2017-10-10 12:12:49 +0530

    Update addCloud.tf

--- github.com/csc/csc-agility-terraform-provider-plug-in/addCloud.tf
+++ github.com/csc/csc-agility-terraform-provider-plug-in/addCloud.tf
@@ -1,19 +1,13 @@
-variable "agility_userid" {}
-variable "agility_password" {}
-provider "agility" {
-  userid = "${var.agility_userid}"
-  password = "${var.agility_password}"
-}
 resource "agility_addcloudprovider" "AWS" {
-  cloudname = "PM-AWS"
+  cloudname = "${var.cloud_name}"
   description = "Cloud provider created from terraform"
-  cloudtype = "Amazon EC2 Cloud"
-  hostname = "ec2.eu-west-1.amazonaws.com"
-  publickey = "AKIAJI4INTOM6BXDWR3A"
-  privatekey = "WfUf+j2fPB9VGg1qsWJxSMS0FUxnQrW+tSurofSC"
-  awsaccountnumber = "531227887946"
+  cloudtype = "${var.cloud_type}"
+  hostname = "${var.hostname}"
+  publickey = "${var.aws_accesskey}"
+  privatekey = "${var.aws_secretkey}"
+  awsaccountnumber = "${var.aws_accountnumber}"
 }
 resource "agility_synccloudprovider" "SyncAWS"{
-  cloudname ="PM-AWS"
+  cloudname ="AM-AWS"
   depends_on = ["agility_addcloudprovider.AWS"]
-}
+} 
