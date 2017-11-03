94d9eb87fc3a08a5ae4fec728cd95f0da1433288
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-11-03 16:09:14 +0530

    Update resource_agility_license.go

--- github.com/csc/csc-agility-terraform-provider-plug-in/agility/resource_agility_license.go
+++ github.com/csc/csc-agility-terraform-provider-plug-in/agility/resource_agility_license.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"strings"
 	"os"
-	//"encoding/json"
 
 	"github.com/csc/csc-agility-terraform-provider-plug-in/agility/api"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -14,152 +13,18 @@ import (
 
 )
 
-type SMLicense struct{
-	XMLName struct{}    `"SMLicense"`
-	XMLNS 		string `"xmlns,attr,omitempty"`
-	LicenseVersion 	string	`"LicenseVersion"`
-	Licensee		string	`"Licensee"`
-	NumberOfClients	string	`"NumberOfClients"`
-	MaxPermittedInstances	string `"MaxPermittedInstances"`
-	DBQueryType		string	`"DBQueryType"`
-	Signature		string 	`"Signature"`
-}
-type Adapters struct{
-	XMLName struct{}    `"Adapters"`
-	Adapter string		`"Adapter"`
-
-}
-type Modules struct{
-	XMLName struct{}    `"Modules"`
-	Module string		`"Module"`
-}
-type IssueDate struct{
-	XMLName struct{}	`"IssueDate"`
-	IssueDay	string	`"IssueDay"`
-	IssueMonth	string	`"IssueMonth"`
-	IssueYear	string	`"IssueYear"`
-}
-type ExpiryDate struct{
-	XMLName struct{}	`"ExpiryDate"`
-	ExpiryDay	string	`"ExpiryDay"`
-	ExpiryMonth	string	`"ExpiryMonth"`
-	ExpiryYear	string	`"ExpiryYear"`
-}
-type ServerNodeLock struct{
-	XMLName struct{}	`"ServerNodeLock"`
-	Mask	string	`"Mask"`
-	IPAddr	string	`"IPAddr"`
-}
-
 func resourceLicenseUpload() *schema.Resource {
 
 	return &schema.Resource{
 		Create: LicenseUpload,
 		Read: 	LicenseUploadRead,
 		Delete:	LicenseUploadDelete,
-
-
-		Schema: map[string]*schema.Schema{
-			"licenseversion": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"licensee": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-			},
-			"adapter": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: true,
-				ForceNew:	true,
-			},
-			"module": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"issueday": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"issuemonth": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"issueyear": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"expiryday": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"expirymonth": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"expiryyear": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"numberofclients": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"maxpermittedinstances": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"mask": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"ipaddr": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"dbquerytype": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-			"signature": &schema.Schema{
-				Type:     schema.TypeString,
-				Required: 	true,
-				ForceNew:	true,
-
-			},
-		},
 	}
 }
 
 
 func init() {
+	log.Println("opening the conf.json")
 	file, err1 := os.Open("./agility/api/conf.json")
 	if err1 != nil {
 		log.Println("error:", err1)
@@ -180,7 +45,7 @@ func LicenseUpload(ResourceData *schema.ResourceData, meta interface{}) error {
 	}
 	defer f.Close()
 	log.SetOutput(f)
-
+	log.Println("calling licenseUpload api")
 	licenseresponse := api.LicenseUpload(credentials.UserName, credentials.Password)
 	r := strings.NewReader(string(licenseresponse))
 	log.Println("Deploy response is : ", r)
