41c36692b1d5066df46daecff4d11cdfa7562a7f
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-11-03 16:09:40 +0530

    Update agility.tf

--- github.com/csc/csc-agility-terraform-provider-plug-in/agility.tf
+++ github.com/csc/csc-agility-terraform-provider-plug-in/agility.tf
@@ -1,8 +1,8 @@
 variable "agility_userid" {}
 variable "agility_password" {}
 provider "agility" {
-    userid = "${var.agility_userid}"
-    password = "${var.agility_password}"
+  userid = "${var.agility_userid}"
+  password = "${var.agility_password}"
 }
 # Create a new Linux instance on a small server
 /*resource "agility_compute" "serv" {
@@ -16,38 +16,5 @@ provider "agility" {
 }*/
 #Upload a License
 resource "agility_license" "raghib"{
-    licenseversion = "3.0"
-    licensee = "qa_unlimited_allmod_alladapters"
-    adapters = "Amazon EC2 Cloud"
-    adapters = "VMware vSphere Cloud"
-    adapters = "vCloud Director"
-    adapters = "Terremark VCloud Express"
-    adapters = "Savvis"
-    adapters = "Fujitsu"
-    adapters = "KMIP"
-    adapters = "hyperV"
-    adapters = "OpenStack"
-    adapters = "Azure"
-    modules = "Store"
-    modules = "Designer"
-    modules = "Operations"
-    modules = "Planner"
-    modules = "Release Manager"
-    modules = "Admin"
-    modules = "Launch Pad"
-    modules = "Policy"
-    modules = "Reporting"
-    modules = "API"
-    issueday = "11"
-    issuemonth = "12"
-    issueyear = "2012"
-    expiryday = "-1"
-    expirymonth = "-1"
-    expiryyear = "-1"
-    numberofclients = "-1"
-    maxpermittedinstances = "-1"
-    mask = "0.0.0.0"
-    ipaddr = "127.0.0.1"
-    dbquerytype = "DEFAULT"
-    signature = "ew3IKfLOV0XOxjsD+YfVonZMAeI="
+
 }
