4bb5330e967c413d535ca770756cd11001317ce8
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-11-27 11:56:24 +0530

    Update agility.tf

--- github.com/csc/csc-agility-terraform-provider-plug-in/agility.tf
+++ github.com/csc/csc-agility-terraform-provider-plug-in/agility.tf
@@ -1,135 +1,269 @@
-package agility
+variable "agility_userid" {}
+variable "agility_password" {}
+provider "agility" {
+    userid = "${var.agility_userid}"
+    password = "${var.agility_password}"
+}
 
-import (
-	"log"
-	"os"
-	"encoding/json"
-
-	"github.com/csc/csc-agility-terraform-provider-plug-in/agility/api"
-	"github.com/hashicorp/terraform/helper/schema"
-)
-/*type Container struct{
-    XMLName struct{}    `xml:"container"`
-    Name        string `xml:"name"`
-    Description string  `xml:"description"`
+//License upload
+/*resource "agility_license" "unlimited"{
 }*/
 
-func resourceCreateStack() *schema.Resource {
+//Create stack
+/*resource "agility_createstack" "stack"{
+    projectname = "POC"
+    imagename = "SOE-RHEL-6.5"
+    stackname = "SOE-RHEL-6.5-Terraform"
+    stackdescription= "SOE-RHEL-6.5-Terraform"
+    operatingsystem = "Linux"
+}*/
 
-	return &schema.Resource{
-		Create: resourceCreateStackCreate,
-		Read:   resourceCreateStackRead,
-		Update: resourceCreateStackUpdate,
-		Delete: resourceCreateStackDelete,
-
-		Schema: map[string]*schema.Schema{
-			"projectname": &schema.Schema{
-				Type:     schema.TypeString,
-				Required:   true,
-				Computed:   false,
-			},
-			"imagename": &schema.Schema{
-				Type:     schema.TypeString,
-				Required:   true,
-				Computed:   false,
-			},
-			"projectid": &schema.Schema{
-				Type:       schema.TypeString,
-				Computed:   true,
-				ForceNew:   true,
-			},
-			"imageid": &schema.Schema{
-				Type:       schema.TypeString,
-				Computed:   true,
-				ForceNew:   true,
-			},
-			"stackname": &schema.Schema{
-				Type:     schema.TypeString,
-				Required:   true,
-				ForceNew:   true,
-			},
-			"stackdescription": &schema.Schema{
-				Type:     schema.TypeString,
-				Required:   true,
-				ForceNew:   true,
-			},
-			"operatingsystem": &schema.Schema{
-				Type:     schema.TypeString,
-				Required:   true,
-				ForceNew:   true,
-			},
-		},
-	}
+//Add and sync cloud provider
+/*resource "agility_addcloudprovider" "AWS" {
+cloudname = "PM-AWS"
+description = "Cloud provider created from terraform"
+cloudtype = "Amazon EC2 Cloud"
+hostname = "ec2.eu-west-1.amazonaws.com"
+publickey = "AKIAJI4INTOM6BXDWR3A"
+privatekey = "WfUf+j2fPB9VGg1qsWJxSMS0FUxnQrW+tSurofSC"
+awsaccountnumber = "531227887946"
 }
-
-func init(){
-	file, err1 := os.Open("./agility/api/conf.json")
-	if err1 != nil {
-		log.Println("error:", err1)
-	}
-	decoder := json.NewDecoder(file)
-	configuration = Config{}
-	err := decoder.Decode(&configuration)
-	if err != nil {
-		log.Println("error:", err)
-	}
-
-	/*err2 := file.Close()
-	log.Printf("err2: %v\n", err2)*/
+resource "agility_synccloudprovider" "SyncAWS" {
+    cloudname = "PM-AWS"
+    depends_on = [
+        "agility_addcloudprovider.AWS"]
 }
+# Create a new Linux instance on a small server
+/*resource "agility_compute" "myserver" {
+    name = "myserver"
+    active = "true"
+    version = "1"
+    type = "t1.micro"
+    blueprint = "SOE-RHEL-6.7"
+    environment = "Development"
+    project = "StackImportSOE"
+}*/
 
-func resourceCreateStackCreate(ResourceData *schema.ResourceData, meta interface{}) error {
-	credentials = meta.(ProvCredentials)
-	//set up logging
-	f, errf := os.OpenFile("./agility/api/agility.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if errf != nil {
-		log.Println("error opening file: ", errf)
-	}
-	defer f.Close()
-
-	log.SetOutput(f)
-	projectName := ResourceData.Get("projectname").(string)
-	log.Println("the Project name is: ", projectName)
-	response, err := api.GetProjectId(string (projectName), credentials.UserName, credentials.Password)
-	if err != nil {
-		log.Println("Error in getting projectid: ", err)
-		return err
-	}
-	ResourceData.Set("projectid",string(response))
-	log.Println("the projectid is: ", string(response))
-	projectId := ResourceData.Get("projectid").(string)
-
-
-	imageName := ResourceData.Get("imagename").(string)
-	log.Println("the image name is: ", imageName)
-	response1, err := api.GetImageId(string (imageName), credentials.UserName, credentials.Password)
-	if err != nil {
-		log.Println("Error in getting imageid: ", err)
-		return err
-	}
-	ResourceData.Set("imageid",string(response1))
-	log.Println("the imageid is: ", string(response1))
-	imageId := ResourceData.Get("imageid").(string)
-
-
-	api.CreateStack(ResourceData, projectId, imageId, credentials.UserName, credentials.Password)
-	return nil
+//Org Structure
+/*resource "agility_createcontainer" "HRContainer" {
+    parentcontainername = "Terraform"
+    container = "HR"
+    description = "Container created via terraform"
 }
-
-func resourceCreateStackRead(d *schema.ResourceData, meta interface{}) error {
-	// no need to do anything for read state
-
-	return nil
+resource "agility_createcontainer" "FinanceContainer" {
+    parentcontainername = "Terraform"
+    container = "Finance"
+    description = "Container created via terraform"
 }
-
-func resourceCreateStackUpdate(d *schema.ResourceData, meta interface{}) error {
-	// no need to do anything for read state
-
-	return nil
+resource "agility_createcontainer" "ITContainer" {
+    parentcontainername = "Terraform"
+    container = "IT"
+    description = "Container created via terraform"
 }
-
-func resourceCreateStackDelete(d *schema.ResourceData, meta interface{}) error {
-	// no need to do anything for read state
-
-	return nil
+resource "agility_createcontainer" "SubHRContainer" {
+    parentcontainername = "HR"
+    container = "SubHR"
+    description = "Container created via terraform"
+    depends_on = ["agility_createcontainer.HRContainer"]
 }
+resource "agility_createcontainer" "AdminContainer" {
+    parentcontainername = "IT"
+    container = "Admin"
+    description = "Container created via terraform"
+    depends_on = ["agility_createcontainer.ITContainer"]
+}
+resource "agility_createcontainer" "DeveloperContainer" {
+    parentcontainername = "IT"
+    container = "Developer"
+    description = "Container created via terraform"
+    depends_on = ["agility_createcontainer.ITContainer"]
+}
+resource "agility_createproject" "HRProject"{
+    parentcontainername="SubHR"
+    project="HR"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.SubHRContainer"]
+}
+resource "agility_createproject" "FinanceProject"{
+    parentcontainername="Finance"
+    project="Finance"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.FinanceContainer"]
+}
+resource "agility_createproject" "WindowsProject"{
+    parentcontainername="Admin"
+    project="Windows"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.AdminContainer"]
+}
+resource "agility_createproject" "LinuxProject"{
+    parentcontainername="Admin"
+    project="Linux"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.AdminContainer"]
+}
+resource "agility_createproject" "DotNetProject"{
+    parentcontainername="Developer"
+    project="DotNet"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.DeveloperContainer"]
+}
+resource "agility_createproject" "JavaProject"{
+    parentcontainername="Developer"
+    project="Java"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.DeveloperContainer"]
+}
+resource "agility_createproject" "PythonProject"{
+    parentcontainername="Developer"
+    project="Python"
+    description="Project created via terraform"
+    depends_on = ["agility_createcontainer.DeveloperContainer"]
+}
+resource "agility_createenvironment" "DevelopmentHR"{
+    parentprojectname="HR"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.HRProject"]
+}
+resource "agility_createenvironment" "UATHR"{
+    parentprojectname="HR"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.HRProject"]
+}
+resource "agility_createenvironment" "ProductionHR"{
+    parentprojectname="HR"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.HRProject"]
+}
+resource "agility_createenvironment" "DevelopmentFinance"{
+    parentprojectname="Finance"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.FinanceProject"]
+}
+resource "agility_createenvironment" "UATFinance"{
+    parentprojectname="Finance"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.FinanceProject"]
+}
+resource "agility_createenvironment" "ProductionFinance"{
+    parentprojectname="Finance"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.FinanceProject"]
+}
+resource "agility_createenvironment" "DevelopmentWindows"{
+    parentprojectname="Windows"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.WindowsProject"]
+}
+resource "agility_createenvironment" "UATWindows"{
+    parentprojectname="Windows"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.WindowsProject"]
+}
+resource "agility_createenvironment" "ProductionWindows"{
+    parentprojectname="Windows"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.WindowsProject"]
+}
+resource "agility_createenvironment" "DevelopmentLinux"{
+    parentprojectname="Linux"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.LinuxProject"]
+}
+resource "agility_createenvironment" "UATLinux"{
+    parentprojectname="Linux"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.LinuxProject"]
+}
+resource "agility_createenvironment" "ProductionLinux"{
+    parentprojectname="Linux"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.LinuxProject"]
+}
+resource "agility_createenvironment" "DevelopmentDotNet"{
+    parentprojectname="DotNet"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.DotNetProject"]
+}
+resource "agility_createenvironment" "UATDotNet"{
+    parentprojectname="DotNet"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.DotNetProject"]
+}
+resource "agility_createenvironment" "ProductionDotNet"{
+    parentprojectname="DotNet"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.DotNetProject"]
+}
+resource "agility_createenvironment" "DevelopmentJava"{
+    parentprojectname="Java"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.JavaProject"]
+}
+resource "agility_createenvironment" "UATJava"{
+    parentprojectname="Java"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.JavaProject"]
+}
+resource "agility_createenvironment" "ProductionJava"{
+    parentprojectname="Java"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.JavaProject"]
+}
+resource "agility_createenvironment" "DevelopmentPython"{
+    parentprojectname="Python"
+    environment= "Dev"
+    description="Environment created via terraform"
+    environmenttype="DEV"
+    depends_on = ["agility_createproject.PythonProject"]
+}
+resource "agility_createenvironment" "UATPython"{
+    parentprojectname="Python"
+    environment= "UAT"
+    description="Environment created via terraform"
+    environmenttype="UAT"
+    depends_on = ["agility_createproject.PythonProject"]
+}
+resource "agility_createenvironment" "ProductionPython"{
+    parentprojectname="Python"
+    environment= "Production"
+    description="Environment created via terraform"
+    environmenttype="PROD"
+    depends_on = ["agility_createproject.PythonProject"]
+}*/
